814b9a52cab0547604d9147f9208565fbe6e8eec
Rafael Pereira de Paula <91758193+rafael19930509@users.noreply.github.com>
2024-02-03 22:25:14 -0300

    Portfolio 1.0

---
+++ app/components/TimeLine.tsx
@@ -0,0 +1,93 @@
+import angularIcon from '../../public/images/angularIcon.png'
+import csharpIcon from '../../public/images/csharpIcon.png'
+import cssIcon from '../../public/images/cssIcon.png'
+import dotNetIcon from '../../public/images/dotNetIcon.png'
+import htmlIcon from '../../public/images/htmlIcon.png'
+import javascriptIcon from '../../public/images/javascriptIcon.png'
+import nodeIcon from '../../public/images/nodeIcon.png'
+import reactIcon from '../../public/images/reactIcon.png'
+import sqlIcon from '../../public/images/sqlIcon.png'
+
+
+
+
+interface Item {
+    data?: string
+    description?: string
+    title?: string
+    sub?: string
+    tasks?: string[]
+    techs?: string
+}
+
+interface Items {
+    item: Item[]
+
+}
+
+export default function TimeLine({ item }: Items) {
+
+    return (
+
+        <div className="pt-[60px] relative -left-5">
+            {item.length > 0 ? (item.map((it, _index) =>
+
+                <div key={_index.toString()}>
+                    <div>
+
+                        {_index != item.length - 1 ?
+                            <div className="border-l-2  border-green-500">
+                                <div className=' w-[320px] md:w-[700px] h-full'>
+                                    <div className="h-1"></div>
+                                    <div className='bg-[#272831] pl-5 pr-5 pb-5 pt-2 -top-[75px] left-6 relative text-[#b8b8b8] rounded-lg mt-10'>
+                                        <div className='bg-white rounded-full w-3 h-3 relative -left-[51px] top-5'>
+                                        </div>
+                                        <div className="min-[1140px]:-left-[218px] min-[1140px]:absolute top-[18px]">
+                                            <div className="text-sm bg-[#c9c9c9] border-2 border-[#c9c9c9] rounded-md pl-3 pr-3 min-[1140px]:top-[3.4px] -top-2 min-[1140px]:-left-2 -left-1.5 relative text-[#000000] w-44 h-[23px]">{it.data}</div>
+                                            <span className=" text-[#c9c9c9] invisible min-[1140px]:visible text-3xl absolute left-[161px] -top-[4px]">▶</span>
+                                        </div>
+                                        {/* <span className="left-[5px] absolute text-[#c9c9c9] text-2xl top-[16px]">◀</span><span className="bg-[#c9c9c9] rounded-md pl-3 pr-3">{it.data}</span> */}
+                                        <div className="text-white font-medium">{it.title}</div>
+                                        <div className="underline text-[#26c226]">{it.sub}</div>
+                                        <div className="mt-2">{it.description}</div>
+                                        <div className="mt-2">Atividades:</div>
+                                        <ul className='list-disc relative left-5'>
+                                        {it.tasks?.length != null? (it.tasks?.map((task, index) => <li key={index.toString()}>{task}</li>)) : <></>}
+                                        </ul>
+                                        <div className="mt-2">Tecnologias: {it.techs}</div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            :
+                            <>
+                                <div className=" ">
+                                    <div className=' w-[320px] md:w-[700px] h-full'>
+                                        <div className="h-1"></div>
+                                        <div className='bg-[#272831] pl-5 pr-5 pb-5 pt-2 -top-[75px] left-6 relative text-[#b8b8b8] rounded-lg mt-10'>
+                                            <div className='bg-white rounded-full w-3 h-3 relative -left-[49px] top-5'>
+                                            </div>
+                                            <div className="min-[1140px]:-left-[218px] min-[1140px]:absolute top-[18px]">
+                                            <div className="text-sm bg-[#c9c9c9] border-2 border-[#c9c9c9] rounded-md pl-3 pr-3 min-[1140px]:top-[3.4px] -top-2 min-[1140px]:-left-2 -left-1.5 relative text-[#000000] w-44 h-[23px]">{it.data}</div>
+                                            <span className=" text-[#c9c9c9] invisible min-[1140px]:visible text-3xl absolute left-[161px] -top-[4px]">▶</span>
+                                        </div>
+                                        {/* <span className="left-[5px] absolute text-[#c9c9c9] text-2xl top-[16px]">◀</span><span className="bg-[#c9c9c9] rounded-md pl-3 pr-3">{it.data}</span> */}
+                                        <div className="text-white font-medium">{it.title}</div>
+                                        <div className="underline text-[#26c226]">{it.sub}</div>
+                                        <div className="mt-2">{it.description}</div>
+                                        <div className="mt-2">Atividades:</div>
+                                        <ul className='list-disc relative left-5'>
+                                        {it.tasks?.length != null? (it.tasks?.map((task, index) => <li key={index.toString()}>{task}</li>)) : <></>}
+                                        </ul>
+                                        <div className="mt-2">Tecnologias: {it.techs}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        }
+                    </div>
+                </div>
+            )) : <></>}
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,122 @@
+'use client'
+
+import React from 'react';
 import Image from 'next/image'
+import perfil from '../public/images/perfil.jpg'
+import emailIcon from '../public/images/emailIcon.png'
+import gitHubIcon from '../public/images/gitHubIcon.png'
+import linkedinIcon from '../public/images/linkedinIcon.png'
+import TimeLine from '../app/components/TimeLine'
+import Link from 'next/link';
+
 
 export default function Home() {
+
+  const items = [
+    {
+      description:
+        'Empresa que terceriza serviços de programadores que ficam alocados em clientes conforme a demanda. '
+      , data: '10/2020 - Atual...'
+      , title: 'Rerum Engenharia de Sistemas LTDA'
+      , sub: 'Analista Desenvolvedor Full Stack'
+      , tasks:
+        [
+          'Desenlvolvedor técnico de novas funcionalidades/features para o sistema, envolvendo alterações no front-end, no back-end e na base de dados; '
+          , 'Ajuste de bugs detectados pelo time de operações; '
+          , 'Participação das dailys para o acompanhamento/posicionamento sobre o andamento da demanda na sprint; '
+          , 'Controle de versionamento do código fonte através do GitHub/Azure; '
+          , 'Análise de escopo para o levantamento de esforço (tempo de serviço) para o desenvolvimento de uma demanda determinada'
+        ]
+      , techs: 'C#; .NET; SQL Server; GitHub; Azure; Angular; HTML; CSS; Javascript.'
+    }
+    , {
+      description:
+        'Empresa que oferece um sistema de gestão voltado para clinicas de medicina ocupacional. '
+      , data: '03/2019 - 10/2020'
+      , title: 'MSITT (Medical Solution IT)'
+      , sub: 'Analista Desenvolvedor Full Stack'
+      , tasks:
+        [
+          'Deselvolvedor técnico de novas funcionalidades/features para o sistema, envolvendo alterações no front-end, no back-end e na base de dados; '
+          , 'Migração do fonte do sistema legado para uma nova linguagem; '
+          , 'Controle de versionamento do código fonte através do Azure; '
+          , "Desenvolvimento de API's que fazem integração com sistemas terceiros."
+        ]
+      , techs: 'C#; .NET; SQL Server; Azure; JQuery; HTML; CSS; Javascript; ASP Web; Swagger'
+    }
+    , {
+      description:
+        'Portal imobiliário que faz parte do Grupo ZAP, que oferece uma plataforma para corretores e construtoras, auxiliando na divulgação de imóveis usados e lançamentos.'
+      , data: '05/2015 - 01/2018'
+      , title: 'VivaReal'
+      , sub: 'Analista de Suporte'
+      , tasks:
+        [
+          'Análise de inconformidades resultantes da integração entre portais; '
+          , 'Testes unitários para validar bugs e XMLs que estabelecem as atualizações das integrações; '
+          , 'Participação de reuniões com o time de Engenheiros para o apontamento das situações críticas do portal, com maior impacto na usabilidade dos usuários; '
+          , "Levantamento de relatórios extraídos da base, conforme as solicitações por demanda."
+        ]
+      , techs: 'SQL Server; XML'
+    }
+  ]
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='bg-[#333541] h-full main-container'>
+
+      <div className='md:flex justify-center items-center pt-10 md:pt-40 w-full'>
+        <div className=' grid justify-items-center'><Image className=' justify-center items-center rounded-full h-72 w-72 md:h-52 md:w-52' src={perfil} alt={''}></Image></div>
+        <div className='rounded-lg h-[350px] md:h-[300px] p-6 bg-[#272831] m-5'>
+          <div className='text-white md:text-6xl text-2xl  text-center'>Rafael de Paula</div>
+          <div className='text-[#b8b8b8] text-lg top-5 relative'>
+            <div>Analista Desenvolvedor Fullstack</div>
+            <div>.NET - Next JS - JavaScript - SQL Server</div>
+            <div>Buscando por oportunidades freelancer</div>
+            <div className='relative top-8 flex justify-center'>
+              <div className='w-12 h-12 ml-2 mr-2 opacity-65 hover:opacity-100 duration-200 hover:cursor-pointer'>
+                <Link href={'https://www.linkedin.com/in/rafael-de-paula-59666513a/'}><Image src={linkedinIcon} alt=''></Image></Link>
+              </div>
+              <div className='w-12 h-12 ml-2 mr-2 opacity-65 hover:opacity-100 duration-200 hover:cursor-pointer relative top-1'>
+                <Link href={'https://github.com/Rafael9375'}><Image src={gitHubIcon} alt=''></Image></Link>
+              </div>
+              <div className='w-12 h-12 ml-2 mr-2 opacity-65 hover:opacity-100 duration-200 hover:cursor-pointer relative top-3'>
+                <Link href={'mailto: rafael.anexo@gmail.com'}><Image src={emailIcon} alt=''></Image></Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='w-full grid justify-items-center '>
+        <TimeLine item={items} />
+
       </div>
+      <div className='h-[700px] w-full  grid justify-items-center'>
+        <div className='h-[620px] md:w-[700px] rounded-lg justify-items-center bg-[#272831] pt-10 pl-10 pr-10 ml-5 mr-5 '>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className='w-full text-2xl text-white relative'>Clone do Netflix</div>
+          <div className='w-full text-[#26c226] relative underline'>React - Next JS - Node - Tailwind</div>
+          <div className='w-full text-[#b8b8b8] h-24 relative top-10'>
+            Projeto desenvolvido para fins de estudo, sendo um clone do site Netflix com as principais páginas de navegação.
+          </div>
+          <div className='md:w-full md:hidden flex justify-center top-5 relative h-[10] w-[10]'>
+            <iframe className='relative rounded-xl' width="300" height="315" src="https://www.youtube.com/embed/mfD01qS9TRc?si=ByKCEiTXuhBvopDb" title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            </iframe>
+          </div>
+          <div className='max-[767px]:hidden md:w-full flex justify-center top-5 relative h-[10] w-[10]'>
+            <iframe className='relative rounded-xl' width="500" height="315" src="https://www.youtube.com/embed/mfD01qS9TRc?si=ByKCEiTXuhBvopDb" title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+            </iframe>
+          </div>
+          
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
-    </main>
+      <div className='h-10 relative'></div>
+
+    </div>
   )
 }
